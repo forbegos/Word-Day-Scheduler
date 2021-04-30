15ad7a6a857fd45286441fc83cb901030af7cd28
forbegos <forbegos@me.com>
2021-04-30 08:46:26 -0600

    tasks saving and retrieval working. Also did some refactoring to clean code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,25 @@
-// Declarations
-var rowElement = $(".time-block");
-var textAreaEl = $("textarea");
-var buttonEl = $("button");
+var tasks = JSON.parse(localStorage.getItem("tasks"));
 
-// Testing getting text from text area element in each row
-console.log(rowElement.children("textarea").get(0));
-console.log(rowElement.children("textarea").get(1));
-console.log(rowElement.children("textarea").get(2));
-
-// Testing getting id from button
-console.log(buttonEl.get(0).id);
-
-// NEXT: TO saves text from each row, loop array until button id === time then assign desc = textarea"
-
-// Declare array of objects (time and task description) and populate
-var tasks = new Array(9);
-for (let i = 0; i < tasks.length; i += 1) {
-  tasks[i] = { time: rowElement.get(i).id, desc: "Task " + i }; //<--testing
+if (tasks === null) {
+  tasks = [""];
+  for (let i = 0; i < 9; i++) {
+    tasks[i] = "";
+    console.log(tasks);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
+} else {
+  for (let i = 0; i < tasks.length; i++) {
+    $("textarea#" + i).val(tasks[i]);
+  }
 }
 
-//functions to save and retrieve arrays from localstorage
-function saveArray(name, array) {
-  localStorage.setItem("name", JSON.stringify(array));
+function saveArray(event) {
+  // console.log("Saving...");
+  for (let i = 0; i < 9; i += 1) {
+    tasks[i] = $("textarea#" + i).val();
+  }
+  // console.log(tasks);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
-function retrieveArray(name, array) {
-  array = JSON.parse(localStorage.getItem("name"));
-  return array;
-}
-
-// tasks = retrieveArray("tasks", tasks);
-// saveArray("tasks", tasks);
-// console.log(tasks);
+$(".saveBtn").on("click", saveArray);
