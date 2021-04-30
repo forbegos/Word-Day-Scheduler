052c19eeafceb25db323462cd1d6365d544f5694
forbegos <forbegos@me.com>
2021-04-29 21:14:36 -0600

    tested get().id to retrieve id from each time block row

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+// Declarations
+
+// add selectors here
+var rowElement = $(".time-block");
+console.log(rowElement);
+console.log(rowElement.get(8).id);
+
+// Declare object
+var task = {
+  time: "",
+  description: "",
+};
+
+// Declare array of task
+var taskArray = [];
+
+// Populate Array
+
+// for (let i = 0; i < 9; i++) {
+// taskArray[i].task = //id from row 9
+// taskArray[i].description = //text from row 9
+// }
