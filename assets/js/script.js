79d96a1a54172dd98967485ae60faf8617c837a7
forbegos <forbegos@me.com>
2021-04-30 17:14:27 -0600

    current date showing formatted in header

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,35 @@
-var tasks = JSON.parse(localStorage.getItem("tasks"));
+$(document).ready(function () {
+  // ensures DOM is loaded before JS is executed
+  //--------- declarations------------------------
+  var time = new moment();
 
-if (tasks === null) {
-  tasks = [""];
-  for (let i = 0; i < 9; i++) {
-    tasks[i] = "";
-    console.log(tasks);
+  var tasks = JSON.parse(localStorage.getItem("tasks"));
+  //---------- functions ----------
+
+  $("#currentDay").text(moment().format("dddd, MMM Do YYYY"));
+
+  if (tasks === null) {
+    tasks = [""];
+    for (let i = 0; i < 9; i++) {
+      tasks[i] = "";
+      console.log(tasks);
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
+  } else {
+    for (let i = 0; i < tasks.length; i++) {
+      $("textarea#" + i).val(tasks[i]);
+    }
+  }
+
+  function saveArray(event) {
+    // console.log("Saving...");
+    for (let i = 0; i < 9; i += 1) {
+      tasks[i] = $("textarea#" + i).val();
+    }
+    // console.log(tasks);
     localStorage.setItem("tasks", JSON.stringify(tasks));
   }
-} else {
-  for (let i = 0; i < tasks.length; i++) {
-    $("textarea#" + i).val(tasks[i]);
-  }
-}
 
-function saveArray(event) {
-  // console.log("Saving...");
-  for (let i = 0; i < 9; i += 1) {
-    tasks[i] = $("textarea#" + i).val();
-  }
-  // console.log(tasks);
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-}
-
-$(".saveBtn").on("click", saveArray);
+  // ---------- event handlers ----------------
+  $(".saveBtn").on("click", saveArray);
+});
