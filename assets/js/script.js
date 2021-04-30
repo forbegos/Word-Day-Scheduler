250ef21f33eb1100ba44806a30ccdefe111b0c01
forbegos <forbegos@me.com>
2021-04-29 22:58:03 -0600

    Testing getting id from button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,18 @@
 // Declarations
 var rowElement = $(".time-block");
 var textAreaEl = $("textarea");
+var buttonEl = $("button");
 
+// Testing getting text from text area element in each row
 console.log(rowElement.children("textarea").get(0));
 console.log(rowElement.children("textarea").get(1));
 console.log(rowElement.children("textarea").get(2));
+
+// Testing getting id from button
+console.log(buttonEl.get(0).id);
+
+// NEXT: TO saves text from each row, loop array until button id === time then assign desc = textarea"
+
 // Declare array of objects (time and task description) and populate
 var tasks = new Array(9);
 for (let i = 0; i < tasks.length; i += 1) {
