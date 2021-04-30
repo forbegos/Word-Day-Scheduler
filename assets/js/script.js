31eca0cb0eb732da5349507033592e44e5ff779a
forbegos <forbegos@me.com>
2021-04-29 22:09:15 -0600

    defined array of objects and populated

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,21 +2,9 @@
 
 // add selectors here
 var rowElement = $(".time-block");
-console.log(rowElement);
-console.log(rowElement.get(8).id);
 
-// Declare object
-var task = {
-  time: "",
-  description: "",
-};
-
-// Declare array of task
-var taskArray = [];
-
-// Populate Array
-
-// for (let i = 0; i < 9; i++) {
-// taskArray[i].task = //id from row 9
-// taskArray[i].description = //text from row 9
-// }
+// Declare array of objects (time and task description) and populate
+var tasks = new Array(8);
+for (let i = 0; i < tasks.length; i += 1) {
+  tasks[i] = { time: rowElement.get(i).id, desc: "Task " + i };
+}
