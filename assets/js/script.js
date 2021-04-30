fefff2d9726df566d31fb3d01047019cb331f6a7
forbegos <forbegos@me.com>
2021-04-29 22:23:34 -0600

    save and retrieve arrays functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,22 @@
 // Declarations
-
-// add selectors here
 var rowElement = $(".time-block");
 
 // Declare array of objects (time and task description) and populate
-var tasks = new Array(8);
+var tasks = new Array(9);
 for (let i = 0; i < tasks.length; i += 1) {
-  tasks[i] = { time: rowElement.get(i).id, desc: "Task " + i };
+  tasks[i] = { time: rowElement.get(i).id, desc: "Task " + i }; //<--testing
 }
+
+//functions to save and retrieve arrays from localstorage
+function saveArray(name, array) {
+  localStorage.setItem("name", JSON.stringify(array));
+}
+
+function retrieveArray(name, array) {
+  array = JSON.parse(localStorage.getItem("name"));
+  return array;
+}
+
+// tasks = retrieveArray("tasks", tasks);
+// saveArray("tasks", tasks);
+// console.log(tasks);
