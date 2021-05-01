c6e8aff19d6916f89425d2312b35816ac1b534e4
forbegos <forbegos@me.com>
2021-04-30 18:10:00 -0600

    application working

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
-# Word-Day-Scheduler
+# Word-Day-Scheduler - JS Application
 
-# https://forbegos.github.io/Word-Day-Scheduler/
+This application creates a daily calendar that the user can utilize to create a daily task list.
+The application higlights each hour for the workday (9am to 5pm) color-coding the background to show the current hour, the past hours,
+and the future hours.
+
+The user can enter a task in each of the hour sections and the task will be saved to the computer. When the user exits the application,
+all tasks will remain and show up when the user accesses the application once again.
+
+## Following are some snap-shots of the application:
+
+![](./assets/img/shot-1.png)
+
+![](./assets/img/shot-2.png)
+
+## Following is a link to the application
+
+https://forbegos.github.io/Word-Day-Scheduler/
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,36 @@
 $(document).ready(function () {
   // ensures DOM is loaded before JS is executed
   //--------- declarations------------------------
-  var time = new moment();
 
   var tasks = JSON.parse(localStorage.getItem("tasks"));
-  //---------- functions ----------
 
-  $("#currentDay").text(moment().format("dddd, MMM Do YYYY"));
+  // ------------ time manipulation to set corresponding background color---------------
+  var time = moment();
+  // time.set({ h: 12, m: 00 });
+  // console.log(time);
+  var currentHour = time.hour();
+  console.log(currentHour);
+
+  for (var i = 9; i < 18; i++) {
+    if (currentHour == $(".time-block#" + i).attr("id")) {
+      console.log("time checks");
+      $(".time-block#" + i)
+        .children("textarea")
+        .addClass("present");
+    } else if (currentHour > $(".time-block#" + i).attr("id")) {
+      console.log("time checks");
+      $(".time-block#" + i)
+        .children("textarea")
+        .addClass("past");
+    } else {
+      $(".time-block#" + i)
+        .children("textarea")
+        .addClass("future");
+    }
+  }
+
+  //---------- functions ----------
+  $("#currentDay").text(moment().format("dddd MMMM Do, YYYY"));
 
   if (tasks === null) {
     tasks = [""];
